74ddb0ca9b2c9451b8e2c1c0cef9aa67f1397943
Krithika Manohar <kmanohar@Krithikas-MacBook-Pro.local>
2014-06-29 22:02:50 -0700

    Edit Fourier Modes + Import/export
    -added ability to change number of Fourier modes
    -added import/export (interface incomplete but sufficient for testing)
    -added figure displaying latexed equations - only works for scalar and
    2x2 problems

--- spectruw/mainApplication.py
+++ spectruw/mainApplication.py
@@ -4,12 +4,14 @@
 # This software is distributed under the terms of the GNU General Public License
 #
 
-import sys
+
 from Tkinter import *
 from EntryPanel import *
 from SessionPanel import *
 
 import sys
+import csv
+import pdb
 if sys.version_info[0] < 3:
     import Tkinter as Tk
 else:
@@ -20,6 +22,19 @@ if __name__ == '__main__':
     mainPanel = Panel()
     mainPanel.wm_title("SpectruW")
 
-    Button(master=mainPanel, text='New', command=lambda:EntryPanel()).pack()
-    Button(master=mainPanel, text='Import').pack()
+    def _import():
+        filename = filebox.get()
+        reader = csv.DictReader(open(filename,'rb'),delimiter=',')
+        dict =  {row['key']:row['value'] for row in reader}
+        for k,v in dict.iteritems():
+            try:
+                dict[k] = float(v) if '.' in v else int(v)
+            except ValueError:
+                dict[k] = v
+        SessionPanel(dict)
+                
+    Button(master=mainPanel, text='New', command=lambda:EntryPanel()).grid(row=0)
+    Button(master=mainPanel, text='Import',command=_import).grid(row=1)
+    filebox = Entry(master=mainPanel)
+    filebox.grid(row=1,column=1)
     Tk.mainloop()
